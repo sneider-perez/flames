8522aad1e8d0f383c5603d1d9bfceabf4e35b95e
sneider408 <sneider408@hotmail.com>
2020-02-09 17:37:42 -0500

    Create animation transitions

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -8,11 +8,14 @@ public class Player : MonoBehaviour
     public float speed;
 
     private Rigidbody2D rb;
+    private Animator anim;
+
     private Vector2 moveAmount;
 
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -22,6 +25,14 @@ public class Player : MonoBehaviour
         Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         moveAmount = moveInput.normalized * speed;
 
+        if (moveInput != Vector2.zero)
+        {
+            anim.SetBool("isRunning",true);
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
     }
 
     private void FixedUpdate()
